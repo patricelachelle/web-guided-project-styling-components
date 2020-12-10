73e65e864735d4d07d39703641ba353499528e30
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-10 11:45:11 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 // This is for the fake API. Do not delete!
-import { worker } from './api-mocks/browser'
-worker.start()
+import { worker } from "./api-mocks/browser";
+worker.start();
 
-import React from 'react'
-import { render } from 'react-dom'
-import {}
+import React from "react";
+import { render } from "react-dom";
+import { ThemeProvider } from "styled-components";
+import {Router }
 
-import App from './components/App'
+import App from "./components/App";
 
-import './styles.css'
+import "./styles.css";
 
-render(
-  <App />,
-  document.querySelector('#root')
-)
+render(<App />, document.querySelector("#root"));
