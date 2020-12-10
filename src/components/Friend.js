6aa7f084c061c7b1dceab2a1fbfd3c45bfe13f8c
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-10 12:52:32 -0600

    wip

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -29,7 +29,7 @@ const StyledFriend = styled.div`
   &:hover {
     color: ${(pr) => pr.theme.secondaryColor};
     background-color: cyan;
-    transition: all 2.5s ease-in-out;
+    transition: all 0.5s ease-in-out;
   }
 
   button {
